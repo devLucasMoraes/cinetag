35633207c62b331cbecb244ef74c3e0a11f63442
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-10 21:53:44 -0300

    criado componente Card

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,5 +1,6 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 
@@ -10,6 +11,7 @@ export default function Inicio() {
       <Cabecalho />
       <Banner imagem="home"/>
       <Titulo><h1>Um lugar para guardar seus vídeos e filmes</h1></Titulo>
+      <Card id='1' titulo='lucas' capa='https://avatars.githubusercontent.com/u/104109951?v=4' />
       <Rodape />
     </>
   )
