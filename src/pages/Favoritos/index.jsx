56dee26e0c53b292d55689d60294593377e1e4f8
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-11 20:37:59 -0300

    lista de favoritos

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -4,17 +4,18 @@ import React from 'react'
 import Banner from 'components/Banner'
 import Titulo from 'components/Titulo'
 
-import videos from 'json/db.json'
 import Card from 'components/Card'
+import { useFavoritoContext } from 'context/Favoritos'
 
 export default function Favoritos() {
+  const {favorito} = useFavoritoContext();
   return (
     <>
       <Banner imagem="favoritos" />
       <Titulo><h1>Meus favoritos</h1></Titulo>
       <section className={styles.container}>
-        {videos.map(video => {
-          return <Card {...video} key={video.id} />
+        {favorito.map(fav => {
+          return <Card {...fav} key={fav.id} />
         })}
       </section>
     </>
