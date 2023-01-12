aa23840107536e53081afdf30bd79224a08aa00d
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-11 21:17:53 -0300

    criado pagina NaoEncontrada e configurando suas rotas

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Rodape from "components/Rodape";
 import FavoritosProvider from "context/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
+import NaoEncontrada from "pages/NaoEncontrada";
 import Player from "pages/Player";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -18,6 +19,7 @@ export default function AppRoutes() {
             <Route path="/" element={<Inicio />}></Route>
             <Route path="/favoritos" element={<Favoritos />} />
             <Route path="/:id" element={<Player/>} />
+            <Route path="*" element={<NaoEncontrada />} />
           </Routes>
         </FavoritosProvider>
       </Container>
